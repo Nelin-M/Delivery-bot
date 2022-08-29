ede96ed2c731b6094e3ae49fb091310b630eeac1
vyach <manyakinva@gmail.com>
2022-08-29 15:42:16 +0300

    feat: added new files for the project

---
+++ src/packages/bot/filters/__init__.py
@@ -0,0 +1,17 @@
+"""
+Initializing modules
+"""
+
+from aiogram import Dispatcher
+from .only_for_group_members import GroupMember, NotGroupMember
+from .chat_only_with_a_bot import ChatWithABot
+
+
+def setup(dispatcher: Dispatcher):
+    """
+    Registration of filters
+    """
+
+    dispatcher.filters_factory.bind(GroupMember)
+    dispatcher.filters_factory.bind(NotGroupMember)
+    dispatcher.filters_factory.bind(ChatWithABot)
