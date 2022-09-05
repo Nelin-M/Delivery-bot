d811621b5aea9d6ece618c9dd77140f7cf4cac4a
arsy-off <arsimbekov@gmail.com>
2022-09-05 19:40:21 +0600

    feat(keyboards): added new user keyboards, refactored old ones

---
+++ src/packages/bot/keyboards/buttons/profile.py
@@ -0,0 +1,22 @@
+"""
+This module represents user profile ReplyKeyboards
+"""
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+
+
+profile_menu = ReplyKeyboardMarkup(
+    keyboard=[[KeyboardButton("Редактировать профиль"), KeyboardButton("Удалить профиль")], [KeyboardButton("Назад")]],
+    resize_keyboard=True,
+    one_time_keyboard=True,
+)
+
+profile_delete_menu = ReplyKeyboardMarkup(
+    keyboard=[[KeyboardButton("Да"), KeyboardButton("Отменить")]], resize_keyboard=True, one_time_keyboard=True
+)
+
+
+profile_data_confirmation = ReplyKeyboardMarkup(
+    keyboard=[[KeyboardButton("Всё верно"), KeyboardButton("Хочу исправить")], [KeyboardButton("Отмена")]],
+    resize_keyboard=True,
+    one_time_keyboard=True,
+)
