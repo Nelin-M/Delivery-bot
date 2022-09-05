e9b81a5aaea2c9fc033030b06adfffdcd60fb6e0
Maxim Nelin <nelin.max0@gmail.com>
2022-09-05 12:42:43 +0600

    fix: typos and minor errors

--- src/packages/database/shemas.py
+++ src/packages/database/shemas.py
@@ -2,23 +2,26 @@
     The file to create database tables
 """
 
-from sqlalchemy import Table, Column, Integer, String, Date, Time, sql, inspect, \
-    ForeignKey
-from typing import List
+from sqlalchemy import Column, Integer, String, Date, Time, sql, ForeignKey
 from gino import Gino
 
 db = Gino()
 
 
 class BaseModel(db.Model):
+    """
+    The class of Base Model
+    """
+
     __abstract__ = True
 
 
 class Car(BaseModel):
     """
-        The class to create car table
+    The class to create car table
     """
-    __tablename__ = 'cars'
+
+    __tablename__ = "cars"
     id = Column(Integer(), primary_key=True)
     model = Column(String(30), nullable=False)
     brand = Column(String(30), nullable=False)
@@ -28,9 +31,10 @@ class Car(BaseModel):
 
 class TgProfile(BaseModel):
     """
-        The class to create tg_profile table
+    The class to create tg_profile table
     """
-    __tablename__ = 'tg_profiles'
+
+    __tablename__ = "tg_profiles"
     tg_id = Column(Integer(), primary_key=True, autoincrement=False)
     nickname = Column(String(50), nullable=False)
     query: sql.select
@@ -38,25 +42,27 @@ class TgProfile(BaseModel):
 
 class User(BaseModel):
     """
-        The class to create users table
+    The class to create users table
     """
-    __tablename__ = 'users'
+
+    __tablename__ = "users"
     id = Column(Integer(), primary_key=True)
-    tg_id = Column(Integer(), ForeignKey('tg_profiles.tg_id'), nullable=False, unique=True)
+    tg_id = Column(Integer(), ForeignKey("tg_profiles.tg_id"), nullable=False, unique=True)
     first_name = Column(String(50))
     last_name = Column(String(50))
-    car_id = Column(Integer(), ForeignKey('cars.id'))
+    car_id = Column(Integer(), ForeignKey("cars.id"))
     phone_number = Column(String(10))
     query: sql.select
 
 
 class RideRequest(db.Model):
     """
-        The class to create requests table
+    The class to create ride requests table
     """
-    __tablename__ = 'ride_requests'
+
+    __tablename__ = "ride_requests"
     id = Column(Integer(), primary_key=True)
-    author = Column(Integer(), ForeignKey('users.id'), nullable=False)
+    author = Column(Integer(), ForeignKey("users.id"), nullable=False)
     delivery_terms = Column(String())
     date = Column(Date(), nullable=False)
     time = Column(Time(), nullable=False)
