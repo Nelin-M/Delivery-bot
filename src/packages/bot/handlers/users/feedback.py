edfa3df6ca98b4e0d9bc6c9e1d6c6f7329c38a71
vyach <manyakinva@gmail.com>
2022-10-04 18:06:08 +0300

    feat: add text in file feedback

--- src/packages/bot/handlers/users/feedback.py
+++ src/packages/bot/handlers/users/feedback.py
@@ -27,12 +27,7 @@ async def write_review(call: types.CallbackQuery):
     """
     Reaction to the text in inline button "Оставить отзыв" and running the state machine
     """
-    group_parameters = await call.bot.get_chat(env_variables.get("CHANNEL_FEEDBACK_ID"))
-    await call.message.answer(
-        f"Ваш отзыв будет опубликован в группе [{group_parameters.title}]({group_parameters.invite_link}), "
-        "ждем предложений и пожеланий:",
-        parse_mode=types.ParseMode.MARKDOWN,
-    )
+    await call.message.answer("Ваш отзыв будет опубликован в группе, ждем предложений и пожеланий:")
     await CreateReview.review_text.set()
 
 
@@ -41,10 +36,14 @@ async def send_review_in_group(message: types.Message, state: FSMContext):
     """
     Shutting down the state machine and sending a message to the feedback chat
     """
+    group_parameters = await message.bot.get_chat(env_variables.get("CHANNEL_FEEDBACK_ID"))
     answer = f"{message.text}\n\n[{message.from_user.username}]({message.from_user.url})"
     await bot.send_message(env_variables.get("CHANNEL_FEEDBACK_ID"), answer, parse_mode=types.ParseMode.MARKDOWN)
     await message.answer(
         "Спасибо, что оставили, отзыв о нашем сервисе, мы постараемся учесть все замечания и "
-        "обязательно их добавить в следующем обновлении, чтобы стать ещё удобнее для вас!"
+        "обязательно их добавить в следующем обновлении, чтобы стать ещё удобнее для вас!\n\n"
+        f"Ваш отзыв будет опубликован в канале [{group_parameters.title}]({group_parameters.invite_link})!!!",
+        parse_mode=types.ParseMode.MARKDOWN,
     )
+
     await state.finish()
