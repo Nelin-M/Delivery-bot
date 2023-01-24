77e4cc891161352c16ea507a893dd74dd5c9e1dd
Nelin Maxim <nelin.max0@gmail.com>
2023-01-24 19:30:25 +0600

    fix: if the link contains md markup characters, then the bot cannot parse the message, in order to avoid this, make such a link in the form of an md link, and not in the form of text

--- src/packages/bot/handlers/users/texts.py
+++ src/packages/bot/handlers/users/texts.py
@@ -4,8 +4,10 @@ Module to keep texts used in bot answers
 from src.packages.loaders import env_variables
 
 group_link = env_variables.get("CHANNEL_LINK")
+# todo: реализовать модуль для выдачи текстов, с указанием разметки,
+#  проверкой на корректность разметки и экранировкой символов, например, символов в ссылках
 MAIN_MENU_AUTHORISED = (
-    f"Созданные внутри бота заявки публикуются в телеграм канале:\nТелеграм: {group_link} \n\n"
+    f"Созданные внутри бота заявки публикуются в [данном]({group_link}) телеграм канале.\n\n"
     f"[Инструкция по созданию заявки]"
     f"({'https://telegra.ph/Instrukciyu-po-ispolzovaniyu-servisa-DeliveryBot-09-06'})\n\n"
     f"Продолжая пользоваться сервисом вы подтверждаете ваше согласие с "
