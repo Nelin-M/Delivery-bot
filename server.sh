622e7ce01ece3f73d249bc479ef563152e3db869
Nelin Maxim <nelin.max0@gmail.com>
2023-02-04 20:01:01 +0600

    fix: add command docker compose start

--- server.sh
+++ server.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
-SESSIONNAME="delivery_bot"
-tmux has-session -t $SESSIONNAME
+SESSION_NAME="delivery_bot"
+tmux has-session -t $SESSION_NAME
 
 if [ $? != 0 ]
  then
- tmux new-session -s $SESSIONNAME -d
- tmux send-keys -t $SESSIONNAME "sleep 15" C-m
- tmux send-keys -t $SESSIONNAME "cd ~/Delivery-bot" C-m
- tmux send-keys -t $SESSIONNAME "/root/.local/share/pypoetry/venv/bin/poetry run python -m src.main" C-m
+ tmux new-session -s $SESSION_NAME -d
+ tmux send-keys -t $SESSION_NAME "docker compose start" C-m
+ tmux send-keys -t $SESSION_NAME "sleep 15" C-m
+ tmux send-keys -t $SESSION_NAME "cd ~/Delivery-bot" C-m
+ tmux send-keys -t $SESSION_NAME "/root/.local/share/pypoetry/venv/bin/poetry run python -m src.main" C-m
 fi
